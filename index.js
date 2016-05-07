66c1df7b610363be6612efba727fad10fb18c3e2
Chinmaya Jena <chinmayajena@Chinmayas-MacBook-Air.local>
2016-05-07 18:36:09 +0530

    added auto linkifying of text

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var linkify = require('linkifyjs');
+require('linkifyjs/plugins/hashtag')(linkify); // optional
+var linkifyHtml = require('linkifyjs/html');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,11 +13,11 @@ var io = require('socket.io')(http);
 
 
 var users = [];
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     socket.emit('entrance', {
         message: 'Welcome to the chat room!'
     });
@@ -22,34 +25,38 @@ io.on('connection', function (socket) {
         message: 'Your ID is #' + socket.id
     });
 
-    
-    
-    socket.on('chat message', function (msg) {
-        io.emit('chat message', socket.nickname+" : "+ msg);
+
+    socket.on('chat message', function(msg) {
+
+
+        var linkifiedMsg = linkifyHtml(msg, {
+            defaultProtocol: 'https'
+        });
+        io.emit('chat message', socket.nickname + " : " + linkifiedMsg);
     });
 
 
-    socket.on('join', function (name) {
+    socket.on('join', function(name) {
         console.log(name);
         users.push(name);
         socket.nickname = name;
         io.emit('chat message', 'Hello ' + name)
-           
+
         // attempt to clean up
-        socket.once('disconnect', function () {
+        socket.once('disconnect', function() {
             var pos = users.indexOf(name);
 
             if (pos >= 0)
                 users.splice(pos, 1);
         });
-        
-        
+
+
         io.emit('userlist', users);
     });
-    
+
 
 });
 
-http.listen(3000, function () {
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
